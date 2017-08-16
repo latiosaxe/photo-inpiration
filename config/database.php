a925dc7e7e5fdab188c71df71e04132ba047313c
Axel <axel@ktbo.mx>
2017-08-16 11:45:13 -0500

    Little responsive

--- config/database.php
+++ config/database.php
@@ -47,7 +47,6 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-//            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
