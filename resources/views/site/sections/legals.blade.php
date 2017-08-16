c9e1d79983f63494dc2c6e89e3f9b9c95050a079
Axel <axel@ktbo.mx>
2017-08-16 15:45:01 -0500

    Better title

--- resources/views/site/sections/legals.blade.php
+++ resources/views/site/sections/legals.blade.php
@@ -1,5 +1,5 @@
 @extends('site.master')
-@section('title')Photo inspiration | photography is art @stop
+@section('title')Photography Inspiration | photography is art @stop
 @section('description')Best website for photography inspiration, photography is art, photography is all @stop
 @section('keywords')photography,inspiration,photo @stop
 
